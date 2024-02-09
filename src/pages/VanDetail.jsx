0fa5a1b747a4da51a95faabca9c19e2e55fbd5d3
ngan <vutran@sheridancollege.ca>
2024-02-08 23:17:40 -0500

    Tutorial 17 - Route Params

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
+import "../server"
 
 export function VanDetail() {
-    const id = useParams()
-    console.log(id)
+    const param = useParams()
+
+    async function getVanDetail(vanId) {
+        let res = await fetch("/api/vans")
+        let data = await res.json()
+        return data.vans.find(obj => obj.id == vanId)
+    }
+
+    useEffect(() => {
+        async function printVanDetail(id) {
+            let van = await getVanDetail(id)
+            console.log(van)
+        }
+        printVanDetail(param.id)
+    }, [])
     return <h1>Van Details go here</h1>
 }
