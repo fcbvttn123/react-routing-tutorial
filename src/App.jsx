079ec50d62a7b6270d27492a739e560c85be8294
ngan <vutran@sheridancollege.ca>
2024-02-12 23:18:52 -0500

    Tutorial 21 - Route Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import {Vans} from "./pages/Vans"
 import { VanDetail } from './pages/VanDetail';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/vans" element={<Vans />}/>
-        <Route path="/vans/:id" element={<VanDetail />}/>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VanDetail />}/>
+        </Route>
       </Routes>
       <footer className='bg-zinc-900 h-14 grid place-content-center'>
         <p className='text-gray-400'>Ⓒ 2022 #VANLIFE</p>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,11 @@
+import { Outlet } from "react-router-dom"
+import { Header } from "./Header"
+
 export function Layout() {
-    
+    return (
+        <>
+            <Header />
+            <Outlet />
+        </>
+    )
 }
