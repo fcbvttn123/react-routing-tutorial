5ad3fb94d839b65877de598d6cb99213a2bc3698
ngan <vutran@sheridancollege.ca>
2024-02-15 22:37:29 -0500

    Tutorial 31 - NavLink Exercise

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
     return (
         <nav>
             <div className='w-11/12 h-20 mx-auto flex items-center justify-between'>
                 <div>
-                    <Link className='font-extrabold text-2xl' to="/">#VANLIFE</Link>
+                    <NavLink className='font-extrabold text-2xl' to="/">#VANLIFE</NavLink>
                 </div>
                 <nav className='flex gap-x-5'>
-                    <Link className='font-semibold' to="/host">Host</Link>
-                    <Link className='font-semibold' to="/about">About</Link>
-                    <Link className='font-semibold' to="/vans">Vans</Link>
+                    <NavLink className={(obj) => obj.isActive ? "font-extrabold underline" : "font-semibold"} to="/host">Host</NavLink>
+                    <NavLink className={(obj) => obj.isActive ? "font-extrabold underline" : "font-semibold"} to="/about">About</NavLink>
+                    <NavLink className={(obj) => obj.isActive ? "font-extrabold underline" : "font-semibold"} to="/vans">Vans</NavLink>
                 </nav>
             </div>
         </nav>
