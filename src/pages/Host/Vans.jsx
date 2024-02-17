81e4d5b53f5a2be1d74b2558c9c56abb0320f026
ngan <vutran@sheridancollege.ca>
2024-02-16 22:49:48 -0500

    Tutorial 34

--- src/pages/Host/Vans.jsx
+++ src/pages/Host/Vans.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
 export function Vans() {
+    const [vans, setVans] = useState(null)
+
+    let vanCards = null
+    if(vans) {
+        vanCards = vans.map(obj => (
+            <div key={obj.id}>
+                <Link to={`/host/vans/${obj.id}`}>
+                    <img src={obj.imageUrl} alt="" />
+                    <h1>{obj.name}</h1>
+                    <p>${obj.price}/day</p>
+                </Link>
+            </div>
+        ))
+    }
+
+    async function getVans() {
+        let res = await fetch("/api/vans")
+        let data = await res.json()
+        setVans(data.vans)
+    }
+
+    useEffect(() => {
+        getVans()
+    }, [])
     return (
-        <h1>Host Van List Page</h1>
+        <>
+            <h1>Host Van List Page</h1>
+            {vanCards && vanCards}
+        </>
     )
 }
