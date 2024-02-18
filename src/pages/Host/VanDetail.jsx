6347158331e57d4cb1f621c3711d1e2ac8b8d1f7
ngan <vutran@sheridancollege.ca>
2024-02-17 19:59:02 -0500

    Tutorial 35

--- src/pages/Host/VanDetail.jsx
+++ src/pages/Host/VanDetail.jsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+
 export function VanDetail() {
+    const [vanDetail, setVanDetail] = useState(null)
+    console.log(vanDetail)
+    
+    let {id} = useParams()
+
+    async function getVans() {
+        let res = await fetch("/api/vans")
+        let data = await res.json()
+        let myVan = data.vans.find(obj => obj.id == id)
+        setVanDetail(myVan)
+    }
+
+    useEffect(() => {
+        getVans()
+    }, [])
     return (
-        <h1>Van Detail</h1>
+        <div className="bg-white p-4 mt-10">
+            <div className="flex items-center gap-x-4">
+                <img className="w-36 h-36" src={vanDetail.imageUrl} alt="" />
+                <div>
+                    <button className={`${vanDetail.type == "simple" ? "bg-orange-600" : "bg-green-800"} text-orange-100 text-xl font-semibold px-4 py-1 rounded-md mb-2`}>
+                        {vanDetail.type}
+                    </button>
+                    <h1 className="font-bold text-2xl">{vanDetail.name}</h1>
+                    <p className="text-slate-400 font-semibold">
+                        <span className="text-black font-bold">${vanDetail.price}</span>/day
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
