e8b3349323a85feda7b7f4dc367940d5eaefe809
ngan <vutran@sheridancollege.ca>
2024-02-19 00:14:18 -0500

    Tutorial 37

--- src/pages/Host/VanDetail.jsx
+++ src/pages/Host/VanDetail.jsx
@@ -1,9 +1,8 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 export function VanDetail() {
     const [vanDetail, setVanDetail] = useState(null)
-    console.log(vanDetail)
     
     let {id} = useParams()
 
@@ -18,17 +17,21 @@ export function VanDetail() {
         getVans()
     }, [])
     return (
-        <div className="bg-white p-4 mt-10">
-            <div className="flex items-center gap-x-4">
-                <img className="w-36 h-36" src={vanDetail.imageUrl} alt="" />
-                <div>
-                    <button className={`${vanDetail.type == "simple" ? "bg-orange-600" : "bg-green-800"} text-orange-100 text-xl font-semibold px-4 py-1 rounded-md mb-2`}>
-                        {vanDetail.type}
-                    </button>
-                    <h1 className="font-bold text-2xl">{vanDetail.name}</h1>
-                    <p className="text-slate-400 font-semibold">
-                        <span className="text-black font-bold">${vanDetail.price}</span>/day
-                    </p>
+        vanDetail &&
+        <div className="mt-10"> 
+            <Link className="underline font-semibold" to=".." relative="path">Back to all vans</Link>
+            <div className="bg-white p-4 mt-6">
+                <div className="flex items-center gap-x-4">
+                    <img className="w-36 h-36" src={vanDetail.imageUrl} alt="" />
+                    <div>
+                        <button className={`${vanDetail.type == "simple" ? "bg-orange-600" : "bg-green-800"} text-orange-100 text-xl font-semibold px-4 py-1 rounded-md mb-2`}>
+                            {vanDetail.type}
+                        </button>
+                        <h1 className="font-bold text-2xl">{vanDetail.name}</h1>
+                        <p className="text-slate-400 font-semibold">
+                            <span className="text-black font-bold">${vanDetail.price}</span>/day
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
